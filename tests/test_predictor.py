a50d772e68483c3d6bd376010f7a64bcfefebdd6
martinabeleda <martin.abeleda@gmail.com>
2021-04-25 09:04:25 +1000

    Add pre-processing steps to handle colour and multiscale images

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -8,21 +8,34 @@ from app.predictor import PredictionFunctor, Predictor
 
 
 @pytest.fixture
-def test_predictor() -> Predictor:
-    return Predictor.mnist_model
+def predictor() -> PredictionFunctor:
+    return PredictionFunctor(Predictor.mnist_model)
 
 
 class TestPredictionFunctor:
-    def test_load_predictor(self, test_predictor: Predictor):
-        predictor = PredictionFunctor(test_predictor)
+    def test_load_predictor(self, predictor: PredictionFunctor):
         assert isinstance(predictor, PredictionFunctor)
         assert issubclass(type(predictor.model), tf.keras.models.Model)
         assert predictor.predictor == Predictor.mnist_model
 
     @pytest.mark.parametrize("file", ["3.png", "5.png", "63.png", "65.png", "172.png"])
-    def test_predict(self, file: str, data_dir: Path, test_predictor: Predictor):
-        filepath = data_dir / file
-        predictor = PredictionFunctor(test_predictor)
-        prediction = predictor(filepath)
+    def test_predict(self, file: str, data_dir: Path, predictor: PredictionFunctor):
+        prediction = predictor(data_dir / file)
         assert isinstance(prediction, np.ndarray)
         assert prediction.shape == (1, 10)
+
+    @pytest.mark.parametrize(
+        "file",
+        [
+            "colour_320_232.jpg",
+            "colour_320_240.png",
+            "colour_500_332.jpg",
+            "grayscale_999_893.png",
+        ],
+    )
+    def test_load_image_as_array(self, file: str, data_dir: Path, predictor: PredictionFunctor):
+        image = predictor._load_image_as_array(data_dir / file)
+        assert image.shape == predictor.image_shape
+
+    def test_model_is_grayscale(self, predictor: PredictionFunctor):
+        assert predictor._model_is_grayscale()
